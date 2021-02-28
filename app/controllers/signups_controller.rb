a0a7bf1b7b2e807be98c35ab2435e10fe10b33cf
Bleak Chandler <bleakchandler@gmail.com>
2021-02-28 12:23:49 -0500

    added some changes

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,11 @@
 class SignupsController < ApplicationController
+    def new
+        @signup = Signup.new
+        @campers = Camper.all
+        @activities = Activity.all
+    end
+
+    def time
+        @times = [0..23]
+    end
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,8 @@
 class Signup < ApplicationRecord
   belongs_to :camper
   belongs_to :activity
+
+  def time
+    @times = [0..23]
+  end 
 end
